60768139b0d8f639c5902c8454af26b83eb2538b
Bivash Thakuri <tbivash3@gmail.com>
2021-06-06 14:24:46 -0500

    Add router animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
+import { animate, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnim', [
+      transition('* => *', [
+        query(':enter', [
+          style({
+            background: 'wheat',
+            display: 'block',
+          }),
+          animate(1000),
+        ], { optional: true }),
+        style({
+          background: 'blue'
+        }),
+        animate(1000, style({
+          background: 'violet'
+        })),
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'PersonalDashboard';
+
+  prepareRoute(outlet: RouterOutlet) {
+
+    if (outlet.isActivated)
+      return outlet.activatedRoute.snapshot.url;
+
+    return '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
@@ -20,7 +20,8 @@ import { BookmarkTileComponent } from './bookmark-tile/bookmark-tile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
